9f3bc60e4f2278007c6b294ab45d06fe2895782e
Voodoo30fps <voodoo30fps@gmail.com>
2026-04-01 15:06:18 +0300

    updated the app to use cart context

--- src/App.jsx
+++ src/App.jsx
@@ -70,19 +70,17 @@ function App() {
   const ctxValue = {
     items: shoppingCart.items,
     addItemToCard: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
   };
 
   return (
     <CartContext.Provider value={ctxValue}>
       {/* ^ .Provider is required for React older than 18 */}
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
+      <Header />
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product} />
           </li>
         ))}
       </Shop>
--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -3,5 +3,6 @@ import { createContext } from 'react';
 export const CartContext = createContext({
   items: [],
   addItemToCard: () => {},
+  updateItemQuantity: () => {},
   //   ^ empty funcion for better auto-completion.
 });
